43aadaba5bb6d5a2b717b92f0ae7633c85767347
punyhuman666 <geutom335@gmail.com>
2024-09-25 13:15:01 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f9dc111f-a92f-4e32-875c-86d13b03ea0c';
+export const MODAL_CLOSE_SEC = 2.5;
